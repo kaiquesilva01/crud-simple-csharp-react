954dd2cc7c50a3b9a15c006d07ebaca67543468f
Kaique <kaique.andruid@gmail.com>
2024-06-13 13:09:44 -0300

    Entity User

---
+++ WebKaiqueCrud/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebKaiqueCrud.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Required]
+        public int age { get; set; }
+    }
+}
