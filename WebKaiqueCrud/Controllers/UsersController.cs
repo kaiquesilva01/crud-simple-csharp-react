a657046615c0fc59610968b9c1be08c8b1fd3d13
Kaique <kaique.andruid@gmail.com>
2024-06-14 17:45:01 -0300

    GetUsers

---
+++ WebKaiqueCrud/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebKaiqueCrud.Models;
+using WebKaiqueCrud.Services;
+
+namespace WebKaiqueCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IAsyncEnumerable<User>>> GetUsers()
+        {
+            try
+            {
+                var users = await _userService.GetUsers();
+                return Ok(users);
+            }
+            catch
+            {
+                return BadRequest("Request inválido");
+            }
+        }
+    }
+}
